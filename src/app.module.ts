ce05be80f40839aed9cdf260a61200306f7b669f
Fkelv <kelvinfundinjagi@gmail.com>
2022-08-08 18:22:23 +0300

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CompaniesController } from './companies/companies.controller';
+
+@Module({
+  imports: [],
+  controllers: [AppController, CompaniesController],
+  providers: [AppService],
+})
+export class AppModule {}
